9c415c74ab73f2ddbb6a04404b4bbb8ef9f99919
root <root@s482395.srvape.com>
2022-08-02 13:02:34 +0300

    fix category

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -16,23 +16,28 @@ class CategoryController extends Controller
 {
     public function actionIndex($id = 0)
     {
+        $categorys = [];
         $category = Category::findOne(["active" => 1, "id" => $id]);
-        if(!$category) {
-            throw new NotFoundHttpException('Категория не найдена');
-        }
-
-        $categorys = [$category->id];
-        if($category->parent_id == 0) {
-            $category_query =  Category::find()->where(["active" => 1, "parent_id" => $id])->all();
-            foreach ($category_query as $category_item) {
-                $categorys[] = $category_item->id;
+        if($category) {
+            $categorys = [$category->id];
+            if($category->parent_id == 0) {
+                $category_query =  Category::find()->where(["active" => 1, "parent_id" => $id])->all();
+                foreach ($category_query as $category_item) {
+                    $categorys[] = $category_item->id;
+                }
             }
         }
+
+        $where = ["active" => 1];
+        if(count($categorys) != 0) {
+            $where['category_id'] = $categorys;
+        }
+
 
         $user_id = Yii::$app->user->isGuest ? 0 : Yii::$app->user->identity->id;
         $query = Product::find()->joinWith(["favorite" => function ($query) use ($user_id)  {
           $query->onCondition(['product_favorite.user_id' => $user_id]);
-        }])->where(["active" => 1, 'category_id' => $categorys])->andWhere(["<>", "availability_count", 0])->orderBy("product.id DESC");
+        }])->where($where)->andWhere(["<>", "availability_count", 0])->orderBy("product.id DESC");
         $countQuery = clone $query;
         $pages = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => 12]);
         $products = $query->offset($pages->offset)
